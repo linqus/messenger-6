24f058165c74cd563fc43d424401a11f650a4f81
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-30 18:46:05 +0200

    dispatch query, get results from handler and use in template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Message\Query\GetTotalImageCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
 
     #[Route('/')]
-    public function homepage()
+    public function homepage(MessageBusInterface $queryBus)
     {
-        return $this->render('main/homepage.html.twig');
+        $envelope = $queryBus->dispatch(new GetTotalImageCount());
+        $stamp =$envelope->last(HandledStamp::class);
+        return $this->render('main/homepage.html.twig', [
+            'imageCount' => $stamp->getResult(),
+        ]);
     }
 }
